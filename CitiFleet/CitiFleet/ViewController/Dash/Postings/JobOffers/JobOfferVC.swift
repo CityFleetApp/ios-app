a22ffa74da3f2b7b6bd3d7e47e8a29c3a43c796f
Nick Kibish <kibish@steelkiwi.com>
2016-03-29 22:55:57 +0300

    created UI for job offers

---
+++ CitiFleet/CitiFleet/ViewController/Dash/Postings/JobOffers/JobOfferVC.swift
@@ -0,0 +1,13 @@
+//
+//  JobOfferVC.swift
+//  CitiFleet
+//
+//  Created by Nick Kibish on 3/29/16.
+//  Copyright © 2016 Nick Kibish. All rights reserved.
+//
+
+import UIKit
+
+class JobOfferVC: UITableViewController {
+    
+}
